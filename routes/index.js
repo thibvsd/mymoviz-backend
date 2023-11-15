6785244ca0b741ef03b72e070c72d801f210b98e
Thibaud Veyssade <tveyssade@gmail.com>
2023-11-15 13:32:28 +0100

    l

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const OWM_API_KEY = process.env.OWM_API_KEY;
+console.log(OWM_API_KEY);
+
+router.get('/movies', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}`)
+	.then(response => response.json())
+    .then(data => {
+        res.json({ movies: data });
+    });
+});
+
+module.exports = router;
